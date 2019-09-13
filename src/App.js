9230957ff9bdc88f532e66fc37117f5878e96055
sumi419 <sasgar04@gmail.com>
2019-09-12 20:47:57 -0700

    ✨Import CountContext Provider to App.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
+import useInterval from './static/lib/useInterval';
+import { CountContext } from './contexts/CountContext';
+import Counter from './components/Counter';
+
+import './static/css/App.css';
+
+export default function App() {
+  const [count, setCount] = useState(0);
+
+  useInterval(() => {
+    // Your custom logic here
+    setCount(count + 1);
+  }, 1000);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <CountContext.Provider value={count}>
+        <Counter />
+      </CountContext.Provider>
     </div>
   );
 }
-
-export default App;
